90462e536ce39e6fc6262026218a55351243b9ef
ja153903 <jaimeabbariao@gmail.com>
2025-12-11 15:25:49 -0500

    day 11 solution and day 12 bootstrap

--- src/advent-of-code/years/2025/11.data.ts
+++ src/advent-of-code/years/2025/11.data.ts
@@ -1,6 +1,15 @@
 import { getPathToProblemInput } from "@/utils/advent-of-code";
-import { read, readlines } from "@/utils/file-io";
+import { readlines } from "@/utils/file-io";
 
 export async function getData(isTest = false) {
-	const data = await readlines(getPathToProblemInput("2025", "11", isTest));
+	const lines = await readlines(getPathToProblemInput("2025", "11", isTest));
+	const graph = new Map<string, Set<string>>();
+
+	for (const line of lines) {
+		const [u, vs] = line.split(": ");
+		const set = new Set(vs.split(" "));
+		graph.set(u, set);
+	}
+
+	return graph;
 }
--- src/advent-of-code/years/2025/11.part2.ts
+++ src/advent-of-code/years/2025/11.part2.ts
@@ -1,7 +1,38 @@
 import { getData } from "./11.data";
 
+function dfs(graph: Map<string, Set<string>>, u: string, v: string) {
+	function rec(node: string, cache: Map<string, number>) {
+		if (cache.has(node)) {
+			return cache.get(node) ?? 0;
+		}
+
+		if (node === v) {
+			return 1;
+		}
+
+		let result = 0;
+
+		const neighbors = graph.get(node);
+		if (neighbors) {
+			for (const neighbor of neighbors) {
+				result += rec(neighbor, cache);
+			}
+		}
+
+		cache.set(node, result);
+
+		return result;
+	}
+
+	return rec(u, new Map());
+}
+
 async function solve(isTest = false) {
-	const data = await getData(isTest);
+	const graph = await getData(isTest);
+	let paths = dfs(graph, "svr", "fft");
+	paths *= dfs(graph, "fft", "dac");
+	paths *= dfs(graph, "dac", "out");
+	return paths;
 }
 
 const ans = await solve();
