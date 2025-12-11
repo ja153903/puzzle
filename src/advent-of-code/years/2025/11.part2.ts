4f3c0166cc06a581497c652b4b90c2cea1b781ca
ja153903 <jaimeabbariao@gmail.com>
2025-12-10 19:18:05 -0500

    adding day 11 bootstrap

---
+++ src/advent-of-code/years/2025/11.part2.ts
@@ -0,0 +1,8 @@
+import { getData } from "./11.data";
+
+async function solve(isTest = false) {
+	const data = await getData(isTest);
+}
+
+const ans = await solve();
+console.log(`Advent of Code 2025 - Day 11 - Part 2: ${ans}`);
